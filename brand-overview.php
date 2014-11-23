beade24b76f079f736ebfcad820b02fac07ce982
Jason Halsey <jason@halsey-design.com>
2014-11-23 14:56:28 -0800

    add brand overview page

---
+++ brand-overview.php
@@ -0,0 +1,64 @@
+<?php
+/*
+Template Name: brand-overview
+ */
+
+get_header(); ?>
+
+
+	<div class="brand-container">
+		
+		<div class="hover-tile-outer">
+		  <div class="hover-tile-container">
+		    <div class="hover-tile hover-tile-visible">
+		      Hover
+		    </div>
+		    <div class="hover-tile hover-tile-hidden">
+		      <h4>Hidden Copy</h4>
+		      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde, hic, dolore, labore,provident eligendi fugiat ad exercitationem.</p>
+		    </div>
+		  </div>
+		</div>
+
+		<div class="hover-tile-outer">
+		  <div class="hover-tile-container">
+		    <div class="hover-tile hover-tile-visible">
+		      Hover
+		    </div>
+		    <div class="hover-tile hover-tile-hidden">
+		      <h4>Hidden Copy</h4>
+		      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde, hic, dolore, labore,provident eligendi fugiat ad exercitationem.</p>
+		    </div>
+		  </div>
+		</div>
+
+		<div class="hover-tile-outer">
+		  <div class="hover-tile-container">
+		    <div class="hover-tile hover-tile-visible">
+		      Hover
+		    </div>
+		    <div class="hover-tile hover-tile-hidden">
+		      <h4>Hidden Copy</h4>
+		      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde, hic, dolore, labore,provident eligendi fugiat ad exercitationem.</p>
+		    </div>
+		  </div>
+		</div>
+
+		<div class="hover-tile-outer">
+		  <div class="hover-tile-container">
+		    <div class="hover-tile hover-tile-visible">
+		      Hover
+		    </div>
+		    <div class="hover-tile hover-tile-hidden">
+		      <h4>Hidden Copy</h4>
+		      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde, hic, dolore, labore,provident eligendi fugiat ad exercitationem.</p>
+		    </div>
+		  </div>
+		</div>	
+
+	</div><!-- end .brand-container -->
+
+
+
+
+<?php get_footer(); ?>
