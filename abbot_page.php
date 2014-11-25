07d4ffdfe14fd1e72778407016f2393aeba4c985
Jason Halsey <jason@halsey-design.com>
2014-11-25 10:13:21 -0800

    vertically center brand copy

--- abbot_page.php
+++ abbot_page.php
@@ -13,8 +13,10 @@ get_header(); ?>
 			<div class="container abbot_top">
 				<img src="<?php bloginfo('template_directory'); ?>/images/brand/am_static.jpg" /></a>
 				<div class="brand-content-wrapper">
-					<p>In 2012, Abbot + Main was founded with a fundamental premise: create a collection of wardrobe essentials for men and women focusing on soft-handed fabrications and thoughtful design elements.</p>
-		          	<p>Our story began in Los Angeles, with a combined vision between actor Kellan Lutz and fashion mogul Daniel Guez, to create an iconic brand using the laid-back spirit of the Southern California beach scene, particularly Venice Beach, where Abbot Kinney and Main Street collide.</p>
+					<div class="brand-content">
+						<p>In 2012, Abbot + Main was founded with a fundamental premise: create a collection of wardrobe essentials for men and women focusing on soft-handed fabrications and thoughtful design elements.</p>
+			          	<p>Our story began in Los Angeles, with a combined vision between actor Kellan Lutz and fashion mogul Daniel Guez, to create an iconic brand using the laid-back spirit of the Southern California beach scene, particularly Venice Beach, where Abbot Kinney and Main Street collide.</p>
+		         	</div>
 	         	 </div>
 			</div>
 		</section>
@@ -28,8 +30,10 @@ get_header(); ?>
 	      <section class="module_brand content_brand">
 	        <div class="container abbot_top">
 	        	<div class="brand-content-wrapper">
-		          <p>In 2012, Abbot + Main was founded with a fundamental premise: create a collection of wardrobe essentials for men and women focusing on soft-handed fabrications and thoughtful design elements.</p>
-		          <p>Our story began in Los Angeles, with a combined vision between actor Kellan Lutz and fashion mogul Daniel Guez, to create an iconic brand using the laid-back spirit of the Southern California beach scene, particularly Venice Beach, where Abbot Kinney and Main Street collide.</p>
+		          <div class="brand-content">
+						<p>In 2012, Abbot + Main was founded with a fundamental premise: create a collection of wardrobe essentials for men and women focusing on soft-handed fabrications and thoughtful design elements.</p>
+			          	<p>Our story began in Los Angeles, with a combined vision between actor Kellan Lutz and fashion mogul Daniel Guez, to create an iconic brand using the laid-back spirit of the Southern California beach scene, particularly Venice Beach, where Abbot Kinney and Main Street collide.</p>
+		         	</div>
 	          </div>
 	          <div class="brand-info-container">
 		          	<div class="brand-info">
